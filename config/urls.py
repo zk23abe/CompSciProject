94741aee2815dd8853517ea985ec217e640c4ae6
ZKhal <zk23abe@herts.ac.uk>
2026-01-19 14:39:20 +0000

    added home and about to views

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name= 'home'),
+    path('about/', about, name= 'about'),
 ]
