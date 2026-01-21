382c302bcf6c29090f944915a45a4cff9b263975
ZKhal <zk23abe@herts.ac.uk>
2026-01-21 11:33:06 +0000

    updated home.html to look like more of an admin dashboard

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html")
+
+    dashboard_stats = {
+        "total_students" : 15,
+        "active_students" : 12,
+        "pending_invoices" : 3,
+
+    }
+    #simulate timetable
+    upcoming_lessons = [
+        {"student": "Sarah Jones", "subject": "GCSE Math", "time": "16:00 Today"},
+        {"student": "Mike Ross", "subject": "A-Level Physics", "time": "17:30 Today"},
+        {"student": "John Doe", "subject": "GCSE English", "time": "10:00 Tomorrow"},
+    ]
+
+    #simulate payments and cash flow management
+    recent_invoices = [
+        {"id": "INV001", "student": "Sarah Jones", "amount": "£45.00", "status": "Paid"},
+        {"id": "INV002", "student": "Mike Ross", "amount": "£60.00", "status": "Pending"},
+    ]
+
+    
+    context = {
+        "stats": dashboard_stats,
+        "lessons": upcoming_lessons,
+        "invoices": recent_invoices,
+        "tutor_name": "Zayan" 
+    }
+
+    return render(request, "home.html", context)
 
 def about(request):
     return render(request, "about.html")
