9596435f2ddcbc450dfb7d4a572969fccfcc6dee
ZKhal <zk23abe@herts.ac.uk>
2026-01-22 12:11:24 +0000

    add form for creating lessons from dashboard

--- pages/forms.py
+++ pages/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Student, Invoice
+from .models import Student, Invoice, Lesson
 
 class StudentForm(forms.ModelForm):
     class Meta:
@@ -23,4 +23,14 @@ class InvoiceForm(forms.ModelForm):
             'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
             'due_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'status': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class LessonForm(forms.ModelForm):
+    class Meta:
+        model = Lessonfieds = ['student', 'subject', 'lesson_date','notes']
+        widgets = {
+            'student': forms.Select(attrs={'class': 'form-select'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'lesson_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
